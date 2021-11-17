3eb858262f1bb0b3d1a1f6a24c7f1d15952aaf92
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2021-11-17 14:56:20 +1100

    initial R4 mthd test

--- R/C4_Ready4useArguments.R
+++ R/C4_Ready4useArguments.R
@@ -14,13 +14,14 @@
 #' @slot pkg_1L_chr Package (a character vector of length one)
 #' @slot overwrite_1L_lgl Overwrite (a logical vector of length one)
 #' @slot write_1L_lgl Write (a logical vector of length one)
+#' @slot dissemination_1L_chr Dissemination (a character vector of length one)
 #' @name Ready4useArguments-class
 #' @rdname Ready4useArguments-class
 #' @export Ready4useArguments
 #' @exportClass Ready4useArguments
 Ready4useArguments <- methods::setClass("Ready4useArguments",
 contains = "Ready4useProcessed",
-slots = c(crs_nbr_dbl = "numeric",write_type_1L_chr = "character",processed_fls_dir_1L_chr = "character",imports_chr = "character",path_to_seed_sf_1L_chr = "character",imports_ls = "list",merge_itms_chr = "character",raw_fls_dir_1L_chr = "character",pkg_1L_chr = "character",overwrite_1L_lgl = "logical",write_1L_lgl = "logical"),
+slots = c(crs_nbr_dbl = "numeric",write_type_1L_chr = "character",processed_fls_dir_1L_chr = "character",imports_chr = "character",path_to_seed_sf_1L_chr = "character",imports_ls = "list",merge_itms_chr = "character",raw_fls_dir_1L_chr = "character",pkg_1L_chr = "character",overwrite_1L_lgl = "logical",write_1L_lgl = "logical",dissemination_1L_chr = "character"),
 prototype =  list(crs_nbr_dbl = NA_real_))
 
 
