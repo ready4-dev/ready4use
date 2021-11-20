a6cf2c8d9686373e9f783b5da6d566bff721b704
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2021-11-20 20:38:33 +1100

    added readhy4show dpcy

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -19,7 +19,7 @@ pkg_desc_ls <- ready4fun::make_pkg_desc_ls(pkg_title_1L_chr = "Retrieve, Manage 
                                                         "https://github.com/ready4-dev/ready4use",
                                                         "https://ready4-dev.github.io/ready4/"))
 x_ready4fun_manifest <- pkg_desc_ls %>%
-  ready4fun::make_manifest(addl_pkgs_ls = ready4fun::make_addl_pkgs_ls(depends_chr = "ready4",
+  ready4fun::make_manifest(addl_pkgs_ls = ready4fun::make_addl_pkgs_ls(depends_chr = "ready4show",
                                                                        suggests_chr = c("rmarkdown","ready4show")),
                            build_ignore_ls = ready4fun::make_build_ignore_ls(file_nms_chr = c("initial_setup.R")),
                            check_type_1L_chr = "ready4",
