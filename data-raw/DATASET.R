305c6612781791bd3f07194841099110c88099fe
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2021-12-22 21:20:05 +1100

    updated ready4 url

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -16,7 +16,7 @@ pkg_desc_ls <- ready4fun::make_pkg_desc_ls(pkg_title_1L_chr = "Author, Retrieve 
                                            ),
                                            urls_chr = c("https://ready4-dev.github.io/ready4use/",
                                                         "https://github.com/ready4-dev/ready4use",
-                                                        "https://ready4-dev.github.io/ready4/"))
+                                                        "https://www.ready4-dev.com/"))
 x <- pkg_desc_ls %>%
   ready4fun::make_manifest(addl_pkgs_ls = ready4fun::make_addl_pkgs_ls(depends_chr = "ready4",
                                                                        suggests_chr = c("rmarkdown")),
