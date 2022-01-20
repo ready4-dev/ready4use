f1c2af238533795f7d276ec7379507d86259c1f7
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2022-01-20 16:20:10 +1100

    modified tb_r3 renew

--- R/mthd_renew.R
+++ R/mthd_renew.R
@@ -13,9 +13,8 @@
 #' @rdname renew-methods
 #' @export 
 #' @importFrom lifecycle is_present deprecate_warn
+#' @importFrom rlang current_env
 #' @importFrom ready4 update_tb_r3 add_lups renew
-#' @importFrom dplyr bind_rows
-#' @importFrom tibble tibble
 renew.ready4use_dictionary <- function (x, var_nm_chr = NA_character_, var_ctg_chr = NA_character_, 
     var_desc_chr = NA_character_, var_type_chr = NA_character_, 
     filter_cdn_1L_chr = NA_character_, new_cases_r3 = NULL, new_ready4_dict_r3 = deprecated(), 
@@ -25,11 +24,10 @@ renew.ready4use_dictionary <- function (x, var_nm_chr = NA_character_, var_ctg_c
         lifecycle::deprecate_warn("0.0.0.9211", "ready4use::renew.ready4use_dictionary(new_ready4_dict_r3)", 
             details = "Please use `ready4use::renew.ready4use_dictionary(new_cases_r3)` instead.")
     }
+    fn_env_ls <- as.list(rlang::current_env())[-1]
     x <- ready4::update_tb_r3(x, filter_cdn_1L_chr = filter_cdn_1L_chr, 
+        fn = renew.ready4use_dictionary, fn_env_ls = fn_env_ls, 
         slice_idxs_int = slice_idxs_int)
-    x <- dplyr::bind_rows(x, tibble::tibble(var_nm_chr = var_nm_chr, 
-        var_ctg_chr = var_ctg_chr, var_desc_chr = var_desc_chr, 
-        var_type_chr = var_type_chr))
     if (!is.null(new_cases_r3)) {
         x <- ready4::add_lups(x, new_lup = new_cases_r3, key_var_nm_1L_chr = "var_nm_chr")
     }
@@ -55,9 +53,8 @@ methods::setMethod("renew", methods::className("ready4use_dictionary", package =
 #' @return x (An object)
 #' @rdname renew-methods
 #' @export 
+#' @importFrom rlang current_env
 #' @importFrom ready4 update_tb_r3 renew
-#' @importFrom dplyr bind_rows
-#' @importFrom tibble tibble
 #' @importFrom purrr reduce
 renew.ready4use_imports <- function (x, local_file_src_chr = NA_character_, path_to_make_script_chr = NA_character_, 
     download_url_chr = NA_character_, inc_file_main_chr = NA_character_, 
@@ -65,12 +62,10 @@ renew.ready4use_imports <- function (x, local_file_src_chr = NA_character_, path
     filter_cdn_1L_chr = NA_character_, local_to_url_vec_chr = NA_character_, 
     slice_idxs_int = NA_integer_, urls_vec_chr = NA_character_) 
 {
+    fn_env_ls <- as.list(rlang::current_env())[-1]
     x <- ready4::update_tb_r3(x, filter_cdn_1L_chr = filter_cdn_1L_chr, 
+        fn = renew.ready4use_imports, fn_env_ls = fn_env_ls, 
         slice_idxs_int = slice_idxs_int)
-    x <- dplyr::bind_rows(x, tibble::tibble(local_file_src_chr = local_file_src_chr, 
-        path_to_make_script_chr = path_to_make_script_chr, download_url_chr = download_url_chr, 
-        inc_file_main_chr = inc_file_main_chr, inc_fls_to_rename_ls = list(), 
-        new_nms_for_inc_fls_ls = list()))
     if (!is.na(local_to_url_vec_chr) & !is.na(urls_vec_chr)) 
         x <- purrr::reduce(1:length(local_to_url_vec_chr), .init = x, 
             ~update_tb_src_loc_to_url_sngl_tb(x = .x, y = .y, 
--- data-raw/mthds/renew.R
+++ data-raw/mthds/renew.R
@@ -12,14 +12,12 @@ renew.ready4use_dictionary <- function(x,
                               "ready4use::renew.ready4use_dictionary(new_ready4_dict_r3)",
                               details = "Please use `ready4use::renew.ready4use_dictionary(new_cases_r3)` instead.")
   }
+  fn_env_ls <- as.list(rlang::current_env())[-1]
   x <- ready4::update_tb_r3(x,
                             filter_cdn_1L_chr = filter_cdn_1L_chr,
+                            fn = renew.ready4use_dictionary, ## ## ##
+                            fn_env_ls = fn_env_ls,
                             slice_idxs_int = slice_idxs_int)
-  x <- dplyr::bind_rows(x,
-                        tibble::tibble(var_nm_chr = var_nm_chr,
-                                       var_ctg_chr = var_ctg_chr,
-                                       var_desc_chr = var_desc_chr,
-                                       var_type_chr = var_type_chr))
   if(!is.null(new_cases_r3)){
     x <- ready4::add_lups(x,
                           new_lup = new_cases_r3,
@@ -38,16 +36,12 @@ renew.ready4use_imports <- function(x,
                                     local_to_url_vec_chr = NA_character_,
                                     slice_idxs_int = NA_integer_,
                                     urls_vec_chr = NA_character_){
+  fn_env_ls <- as.list(rlang::current_env())[-1]
   x <- ready4::update_tb_r3(x,
                             filter_cdn_1L_chr = filter_cdn_1L_chr,
+                            fn = renew.ready4use_imports, ## ## ##
+                            fn_env_ls = fn_env_ls,
                             slice_idxs_int = slice_idxs_int)
-  x <- dplyr::bind_rows(x,
-                        tibble::tibble(local_file_src_chr = local_file_src_chr,
-                                       path_to_make_script_chr = path_to_make_script_chr,
-                                       download_url_chr = download_url_chr,
-                                       inc_file_main_chr = inc_file_main_chr,
-                                       inc_fls_to_rename_ls =list(),
-                                       new_nms_for_inc_fls_ls = list()))
   if(!is.na(local_to_url_vec_chr) & !is.na(urls_vec_chr))
     x <- purrr::reduce(1:length(local_to_url_vec_chr),
                 .init = x,
